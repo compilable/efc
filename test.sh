d4ca02a69856487aed4a784ed3380ee9b26a9866
AeronC <aeron.chamarasinghe@team.telstra.com>
2021-11-22 23:33:23 +1100

    test added for windows filepath

--- test.sh
+++ test.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-shopt -s globstar
-
-gpg --quiet --yes --batch --passphrase "$2" "$1"  2>> "$1_out"
 
 
-if grep -q failed "$1_out"; then
-  echo "faild"
-fi
 
-rm -f -- "$1_out"
+list_files_with_space(){
+    SAVEIFS=$IFS
+    IFS=$(echo -en "\n\b")
+    # set me
+    FILES="$1/*"
+    for f in $FILES
+    do
+        echo "$f"
+    done
+}
+
+
+list_files_with_space "../Neil Yuen/Neil Yuen/"
+
+
+
+test_decrypt(){
+    shopt -s globstar
+    
+    gpg --quiet --yes --batch --passphrase "$2" "$1"  2>> "$1_out"
+    
+    
+    if grep -q failed "$1_out"; then
+        echo "faild"
+    fi
+    
+    rm -f -- "$1_out"
+}

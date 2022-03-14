47d16cf46f0c9ed465aadf7d0a31a068c0baf765
compilable <compilable@gmx.com>
2022-03-15 01:04:45 +1100

    output filename for enc/dec

--- efc_lib.sh
+++ efc_lib.sh
@@ -8,50 +8,67 @@
 shopt -s globstar
 
 decrypt() {
+    # $1 = fq file name
+    # $2 = password
+    # $3 = delete flag (yes/no)
+    # $4 = output file name [optional]
+
     echo -e ' \t' "decrypting the file: $1"
-    
+
     local return_status=0
-    
-    # capture the output to a temp. file.
-    gpg --quiet --yes --batch --passphrase "$2" "$1" 2>>"$1_out"
-    
-    
+
+    if [ -z "${4}" ]; then
+        # decrypt to the same file name
+        gpg --quiet --yes --batch --passphrase "$2" "$1" 2>>"$1_out"
+    else
+        # decrypt to a given file name
+        gpg --quiet --yes --batch --output "$4" --passphrase "$2" "$1" 2>>"$1_out"
+    fi
+
     if [ -s "$1_out" ]; then
         echo -e ' \t' "invalid key/file is provided!, file is ignored!"
     else
-        
+
         if [ $3 == 'yes' ]; then
             echo -e ' \t' "removing the original file $1"
             rm -rf "$1"
         fi
         return_status=1
     fi
-    
+
     rm -rf "$1_out"
-    
+
     return $return_status
 }
 
 encrypt() {
-    
+    # $1 = fq file name
+    # $2 = password
+    # $3 = delete flag (yes/no)
+    # $4 = output file name [optional]
+
     echo -e ' \t' "encrypting the file: $1"
-    
-    gpg --quiet --yes --batch --passphrase "$2" -c "$1"
-    
+
+    if [ -z "${4}" ]; then
+        # encrypt to the same file name.gpg
+        gpg --quiet --yes --batch --passphrase "$2" -c "$1"
+    else
+        # encrypt to a given file name
+        gpg --quiet --yes --batch --output "$4" --passphrase "$2" -c "$1"
+    fi
+
     if [ $3 == 'yes' ]; then
         echo -e ' \t' "removing the original file: $1"
         rm -rf "$1"
     fi
-    
+
     return 1
 }
 
+remove_zip_after_verify() {
 
-
-remove_zip_after_verify(){
-    
     echo "$1"
-    if ! tar tf "$1" &> /dev/null; then
+    if ! tar tf "$1" &>/dev/null; then
         echo -e "\toriginal DIR won't be deleted, error in archive : $1"
     else
         echo -e "\toriginal DIR will be removed! : $2"
@@ -59,33 +76,31 @@ remove_zip_after_verify(){
     fi
 }
 
-zip_all_folders(){
-    
+zip_all_folders() {
+
     find "$1" -print |
-    while read file; do
-        
-        if [[ -d "$file" ]] && [[  "$file" != "$1" ]]; then
-            tar_name=$(basename "$file")
-            tar_fq_name="$1/$tar_name.tar.gz"
-            
-            echo -e "Found : DIR  $file , archiving as : $tar_fq_name"
-            tar -cvf "$tar_fq_name" "$file"
-            
-            remove_zip_after_verify "$tar_fq_name" "$file"
-        fi
-        
-    done
+        while read file; do
+
+            if [[ -d "$file" ]] && [[ "$file" != "$1" ]]; then
+                tar_name=$(basename "$file")
+                tar_fq_name="$1/$tar_name.tar.gz"
+
+                echo -e "Found : DIR  $file , archiving as : $tar_fq_name"
+                tar -cvf "$tar_fq_name" "$file"
+
+                remove_zip_after_verify "$tar_fq_name" "$file"
+            fi
+
+        done
 }
 
-
-read_version(){
-    while IFS= read -r line; do 
-        if [[ "$line" == ???EFC* ]]
-        then
+read_version() {
+    while IFS= read -r line; do
+        if [[ "$line" == ???EFC* ]]; then
             echo -e "\n$line"
             break
         fi
-    done < CHANGELOG.md
+    done <CHANGELOG.md
 }
 
 print_version_info() {

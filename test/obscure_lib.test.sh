ccbab5b15343aecc0b737e09a346737ea01b4950
compilable <compilable@gmx.com>
2022-03-18 17:52:30 +1100

    test script for obscure_lib.sh

--- test/obscure_lib.test.sh
+++ test/obscure_lib.test.sh
@@ -3,9 +3,20 @@
 source_path=$(pwd)
 source "$source_path/../obscure_lib.sh"
 
-
+# TODO:
+#    update the obscure_lib.sh path to :
+#    source "../efc_lib.sh"
+#
 pword_1="random text-2#"
-test_file="test_data/"
+pword_2="second_pass-2"
+source_location="test_data/"
 test_file_output="test_data/ENC_ASIC-pre-loader"
 
-generate_index_file "$test_file" "$pword_1"
+# encrypt
+construct_index_file "$source_location" "$pword_1"
+reconstruct_index_file "$source_location" "$pword_1"
+process_indexed_content "e" "$pword_2"
+
+# decrypt
+reconstruct_index_file "$source_location" "$pword_1"
+process_indexed_content "d" "$pword_2"

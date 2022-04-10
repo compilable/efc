f43b827380c11c6b2581e72158e511dcff279767
compilable <compilable@gmx.com>
2022-04-10 13:23:48 +1000

    startup script added

--- test/obscure_lib.test.sh
+++ test/obscure_lib.test.sh
@@ -10,7 +10,6 @@ source "$source_path/../obscure_lib.sh"
 pword_1="random text-2#"
 pword_2="second_pass-2"
 source_location="test_data/"
-test_file_output="test_data/ENC_ASIC-pre-loader"
 
 # encrypt
 #construct_index_file "$source_location" "$pword_1"

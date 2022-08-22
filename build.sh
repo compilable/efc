0bf301eb88150c72e15bd1f37b4f526fa3aaca78
compilable <compilable@gmx.com>
2022-08-22 23:52:29 +1000

    version info updated

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
 # build script to release the latest version.
-
 
 extract_version_from_file() {
 
@@ -12,19 +10,17 @@ extract_version_from_file() {
 
         while IFS= read -r line; do
             if [[ "$line" == ???EFC* ]]; then
-                echo -e "\n$line \n## Source : https://github.com/compilable/efc/ \n## License : The MIT License (MIT)\n## Copyright 2 2002 compilable@tuta.io"
+                echo -e "\n$line \n## Source : https://github.com/compilable/efc/ \n## License : The MIT License (MIT)\n## Copyright 2002-2022 compilable@tuta.io"
                 break
             fi
         done <$file
 
     else
-        echo -e "\n## EFC - version : unknown. \n## Source : https://github.com/compilable/efc/\n## License : The MIT License (MIT)\n## Copyright 2 2002 compilable@tuta.io"
-                break
+        echo -e "\n## EFC - version : unknown. \n## Source : https://github.com/compilable/efc/\n## License : The MIT License (MIT)\n## Copyright 2002-2022 compilable@tuta.ioo"
         exit
     fi
 
 }
-
 
 # 1 remove existing bin folder
 rm -rf bin
@@ -39,20 +35,15 @@ cp efc_lib bin
 echo "new version number (vXX.XX.XX): "
 read version
 
-
 # 4 create the minified version
 
-cat efc_lib >> "bin/efc_min"
-cat efc >> "bin/efc_min"
+cat efc_lib >>"bin/efc_min"
+cat efc >>"bin/efc_min"
 
 # 5 marking the script with the version & copy the license and Readme
 
 mv bin/efc "bin/efc_$version"
 mv bin/efc_min "bin/efc_$version.min"
-
-# 6 Add version info to the end of the file
-extract_version_from_file >>"bin/efc_$version"
-extract_version_from_file >>"bin/efc_$version.min"
 
 # Make a copy of the min file to be execcuted by calling the efc command
 cp "bin/efc_$version.min" "bin/efc"
@@ -60,7 +51,15 @@ cp "bin/efc_$version.min" "bin/efc"
 cp License "bin/"
 cp README.md "bin/"
 
-# 6 compress the bin folder into release folder
+# 6 Add version info to the end of all scrip files
+version_info=$(extract_version_from_file)
+echo $version_info
+echo "$version_info" >>"bin/efc_lib"
+echo "$version_info" >>"bin/efc_$version.min"
+echo "$version_info" >>"bin/efc_$version"
+echo "$version_info" >>"bin/efc"
+
+# 7 compress the bin folder into release folder
 
 mkdir -p release
 

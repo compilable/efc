2093aabb16ca5873fdd5ba8e202170c9cd4dac47
compilable <compilable@gmx.com>
2022-08-24 15:01:01 +1000

    build script updated

--- build.sh
+++ build.sh
@@ -25,47 +25,32 @@ extract_version_from_file() {
 # 1 remove existing bin folder
 rm -rf bin
 
-# 2 create a bin foldeer and copy the scripts to the new build folder
+# 2 create a bin folder
 mkdir -p bin
-cp efc bin
-cp efc_lib bin
 
-# 3 rename the efc file to new version : efc_VERSION
-
+# 3 collect new version from user : efc_VERSION
 echo "new version number (vXX.XX.XX): "
 read version
 
-# 4 create the minified version
-
+# 4 minify the efc and lib file content by combining all the sh file content into a single file with the name efc
 cat efc_lib >>"bin/efc_min"
 cat efc >>"bin/efc_min"
 
 # 5 marking the script with the version & copy the license and Readme
+mv bin/efc_min "bin/efc"
 
-mv bin/efc "bin/efc_$version"
-mv bin/efc_min "bin/efc_$version.min"
-
-# Make a copy of the min file to be execcuted by calling the efc command
-cp "bin/efc_$version.min" "bin/efc"
-
+# 6 include README.md , include License
 cp License "bin/"
 cp README.md "bin/"
 
-# 6 Add version info to the end of all scrip files
+# 7 Add version info to the end of the efc scrip file.
 version_info=$(extract_version_from_file)
-echo $version_info
-echo "$version_info" >>"bin/efc_lib"
-echo "$version_info" >>"bin/efc_$version.min"
-echo "$version_info" >>"bin/efc_$version"
+echo -e $version_info
 echo "$version_info" >>"bin/efc"
 
-# 7 compress the bin folder into release folder
-
+# 8 compress the bin folder into release folder
 mkdir -p release
-
 echo "creating the build.."
 tar -czvf "release/efc_$version.tar.gz" bin/
 
-tar -czvf "release/efc_min_$version.tar.gz" bin/efc
-
-echo "build completed for the version : $version"
+echo -e "\nbuild completed for the version : $version"
